c0c59077e2bb11fb64d101fea1619a2936734516
veroppi <vero.lanzillotti@gmail.com>
2026-01-04 01:27:03 +0100

    literally losing my mind rn

---
+++ Assets/Scripts/PoliceManager.cs
@@ -0,0 +1,100 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class PoliceChaseSystem : MonoBehaviour
+{
+    public enum PoliceState
+    {
+        None,
+        Far,
+        Near,
+        VeryNear
+    }
+
+    [Header("Tempi")]
+    [SerializeField] private float timeToFar = 5f;
+    [SerializeField] private float timeToNear = 5f;
+    [SerializeField] private float timeToVeryNear = 5f;
+    [SerializeField] private float timeToGameOver = 5f;
+
+    [Header("Specchietto (URP)")]
+    [SerializeField] private MeshRenderer mirrorRenderer;
+    [SerializeField] private Texture noPoliceTexture;
+    [SerializeField] private Texture policeFarTexture;
+    [SerializeField] private Texture policeNearTexture;
+    [SerializeField] private Texture policeVeryNearTexture;
+
+    private Coroutine chaseRoutine;
+    private PoliceState currentState = PoliceState.None;
+    private Material mirrorMaterial;
+
+    private void Awake()
+    {
+       
+        mirrorMaterial = mirrorRenderer.material;
+        ResetPolice();
+    }
+
+   
+    public void OnPlayerDrink()
+    {
+        if (chaseRoutine != null)
+            StopCoroutine(chaseRoutine);
+
+        chaseRoutine = StartCoroutine(PoliceChaseRoutine());
+    }
+
+    private IEnumerator PoliceChaseRoutine()
+    {
+        ResetPolice();
+
+        yield return new WaitForSeconds(timeToFar);
+        SetState(PoliceState.Far);
+
+        yield return new WaitForSeconds(timeToNear);
+        SetState(PoliceState.Near);
+
+        yield return new WaitForSeconds(timeToVeryNear);
+        SetState(PoliceState.VeryNear);
+
+        yield return new WaitForSeconds(timeToGameOver);
+        TriggerGameOver();
+    }
+
+    private void SetState(PoliceState newState)
+    {
+        currentState = newState;
+
+        switch (newState)
+        {
+            case PoliceState.None:
+                mirrorMaterial.SetTexture("_BaseMap", noPoliceTexture);
+                break;
+
+            case PoliceState.Far:
+                mirrorMaterial.SetTexture("_BaseMap", policeFarTexture);
+                break;
+
+            case PoliceState.Near:
+                mirrorMaterial.SetTexture("_BaseMap", policeNearTexture);
+                break;
+
+            case PoliceState.VeryNear:
+                mirrorMaterial.SetTexture("_BaseMap", policeVeryNearTexture);
+                break;
+        }
+    }
+
+    private void ResetPolice()
+    {
+        SetState(PoliceState.None);
+    }
+
+    private void TriggerGameOver()
+    {
+        
+        SceneManager.LoadScene("gameover");
+    }
+}
